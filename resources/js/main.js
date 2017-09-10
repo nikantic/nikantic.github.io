0ef0f7f4a3fb6493677392b981ae679bb8970821
Nikola Antić <nikantic@outlook.com>
2017-09-10 23:01:15 +0200

    add externalLink button to thumbnail and hover handling for touch devices

--- resources/js/main.js
+++ resources/js/main.js
@@ -23,6 +23,29 @@ jQuery(document).ready(function($){
         }
     };
 
+    // function to determine tap outside of thumbnail
+    var tapOutside = function(event){
+        var isTapInside = $.contains($(".thumbnail"), event.target);
+
+        if (!isTapInside) {
+            $('.thumbnail').find(".thumbnailLink").removeClass('thumbnailLinkHover');
+            $('.thumbnail').find(".thumbnailImage").removeClass('thumbnailImageHover');
+            $('.thumbnail').find(".thumbnailAnchor").removeClass('thumbnailLineHover');
+            document.removeEventListener("touchstart", tapOutside, true);
+        }
+    };
+
+    // handling thumbnail hover on touch devices
+    $('.thumbnail').on('touchstart', function(){ 
+        $('.thumbnail').not(this).find(".thumbnailLink").removeClass('thumbnailLinkHover');
+        $('.thumbnail').not(this).find(".thumbnailImage").removeClass('thumbnailImageHover');
+        $('.thumbnail').not(this).find(".thumbnailAnchor").removeClass('thumbnailLineHover');
+        $(this).find(".thumbnailLink").addClass('thumbnailLinkHover');
+        $(this).find(".thumbnailImage").addClass('thumbnailImageHover');
+        $(this).find(".thumbnailAnchor").addClass('thumbnailLineHover');
+        document.addEventListener("touchstart", tapOutside, true);
+    });
+
     // scroll to and animate function
     $(function() {
         $(".navLinks li").bind("click", function(){
