67a566506f9bf0a4d4331e127a67d36c5157d48d
Nikola Antić <nikantic@outlook.com>
2020-09-17 21:08:24 +0200

    Updates

---
+++ precache-manifest.8f8b153b41a44111ed8843a9f23d27e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b3a4ecb5e1d714ac18805afd43dce9",
+    "url": "/nikantic.github.io/index.html"
+  },
+  {
+    "revision": "259e7d3e16d54dfdb6ef",
+    "url": "/nikantic.github.io/static/css/main.b8c2a795.chunk.css"
+  },
+  {
+    "revision": "a4367613a25d3206eb2a",
+    "url": "/nikantic.github.io/static/js/2.ff04c141.chunk.js"
+  },
+  {
+    "revision": "3377ffda74687e7745ec38a7bdbe5cdb",
+    "url": "/nikantic.github.io/static/js/2.ff04c141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "259e7d3e16d54dfdb6ef",
+    "url": "/nikantic.github.io/static/js/main.9543b18b.chunk.js"
+  },
+  {
+    "revision": "f4f826085a02bf8cb95a",
+    "url": "/nikantic.github.io/static/js/runtime-main.698b52b2.js"
+  }
+]);
