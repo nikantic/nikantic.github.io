19559e4a59a1bc2ed2fab7d702bfa422cbacce20
Nikola Antić <nikantic@outlook.com>
2020-09-19 15:33:08 +0200

    Updates

--- precache-manifest.038bc7b0a87a7416a14b6f47877e9376.js
+++ precache-manifest.038bc7b0a87a7416a14b6f47877e9376.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "261015e851a84e1546b90a45c130774f",
+    "revision": "8fef81d2314aa6257bf95644d4cfb7c8",
     "url": "/nikantic.github.io/index.html"
   },
   {
-    "revision": "d4ec4786ae839f875046",
-    "url": "/nikantic.github.io/static/css/main.5ff9d278.chunk.css"
+    "revision": "5bd0296cb7882a578b71",
+    "url": "/nikantic.github.io/static/css/main.3c8f7268.chunk.css"
   },
   {
     "revision": "b66af22220d1478d2b7d",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/nikantic.github.io/static/js/2.cab611b6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d4ec4786ae839f875046",
-    "url": "/nikantic.github.io/static/js/main.89415f11.chunk.js"
+    "revision": "5bd0296cb7882a578b71",
+    "url": "/nikantic.github.io/static/js/main.d23cdab0.chunk.js"
   },
   {
     "revision": "f4f826085a02bf8cb95a",
